e032ebe7882356644541459dadf6273b7cc66cd3
Ubuntu <quadz@quadz-customer-prod-vm.3xdq42kznk1uvikbdhyj0m0n2a.rx.internal.cloudapp.net>
2024-11-14 06:15:45 +0000

    deployment through terraform

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000; // You can choose any available port
- 
+
 app.get('/', (req, res) => {
-  res.send('Hello, Express! app development');
+  res.send('Hello, Express! Application successfully deployed through Terraform.');
 });
- 
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-
