71fc34ef8405d074d48f36d0bfaa7507aa48979c
Ubuntu <quadz@quadz-customer-prod-vm.3xdq42kznk1uvikbdhyj0m0n2a.rx.internal.cloudapp.net>
2024-11-15 09:57:38 +0000

    deployment through terraform

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "rg-id" {
   value = azurerm_resource_group.tf-rg.id
 }
-output "vm_ips" {
-    value = azurerm_public_ip.tf-Pub[*].ip_address 
+output "vm_ip" {
+    value = azurerm_public_ip.tf-Pub[0].ip_address 
 }
