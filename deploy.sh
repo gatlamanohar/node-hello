34759680e5ae80682be8b8dd1f7232ac5b7cbfe4
gatlamanohar <manohar.g@arthink.ai>
2024-11-26 12:29:48 +0530

    ssh testing

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Variables passed from Terraform
+# EMAIL=$1
+# DOMAIN=$2
+
+# Install necessary packages
+sudo apt update
+sudo apt install -y nginx certbot python3-certbot-nginx
+sudo apt install -y curl
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo apt install -y nodejs
+
+# # Run certbot to obtain and install the certificate
+# sudo certbot --nginx --redirect --agree-tos --email "$EMAIL" -d "$DOMAIN"
